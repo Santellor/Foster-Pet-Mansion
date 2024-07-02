ac27d167502e0066323bbbdc81c7305e98e2bb90
Koa Graham <koagraham233@gmail.com>
2024-07-02 11:03:06 -0600

    login/register, redux and router set up

---
+++ src/components/Register.jsx
@@ -0,0 +1,75 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+export default function Register({ setShowRegister}) {
+  //state variables
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  //action declarations
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  //handle register function
+  const handleRegister = async (e) => {
+    e.preventDefault()
+
+    const body = {
+      username: username,
+      email: email,
+      password: password
+    }
+
+    axios.post('/api/register', body)
+    .then((res) => {
+      if (res.data.success) {
+
+        dispatch({
+          type: "USER_AUTH",
+          payload: res.data.userId
+        })
+
+        setShowRegister(false)
+        setUsername("")
+        setPassword("")
+        setEmail("")
+        navigate("/mansion")
+      }
+    })
+  }
+
+  //html rendering
+  return (
+    <div id='register'>
+      <form onSubmit={handleRegister}>
+        <h1>Register</h1>
+
+        <input
+          type='text'
+          placeholder='Username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type='text'
+          placeholder='Email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type='password'
+          placeholder='Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <input type='submit' value='Register'/>
+      </form>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
+//dependency imports
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import store from './redux/store.js'
+import { Provider } from 'react-redux'
+
+//page imports
+import Login from './components/Login.jsx'
 import './index.css'
+
+//browser router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+        <Route index element={<Login />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
-)
+);
